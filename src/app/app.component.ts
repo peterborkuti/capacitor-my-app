bbe28fd6a9e4c8d35d80fed429abe2e8c2112d6d
Peter Borkuti <peterborkuti@gmail.com>
2019-01-31 11:35:42 +0100

    js side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+import * as BP from 'bp-add';
+
+const {  } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  num = 0;
+
+  ngOnInit(): void {
+    BP.Add.add({a: 1, b: 2}).
+      then(value => {
+        console.log((value));
+        this.num = value.value;
+      });
+  }
 }
