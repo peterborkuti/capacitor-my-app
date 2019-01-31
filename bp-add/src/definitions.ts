2c3bb09c85f91d023b3ef7ee151ae07e6cba36c5
Peter Borkuti <peterborkuti@gmail.com>
2019-01-31 10:42:02 +0100

    after installing capacitor-cli globally I could make plugin in other directory

--- bp-add/src/definitions.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  interface PluginRegistry {
-    AddPlugin?: AddPluginPlugin;
-  }
-}
-
-export interface AddPluginPlugin {
-  add(options: { a: number, b: number }): Promise<{value: number}>;
-}
